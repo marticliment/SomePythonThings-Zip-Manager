591d66eb8ff14425b37c1c0caeb271691bc9e827
Martí Climent <53119851+martinet101@users.noreply.github.com>
2021-05-30 17:27:17 +0200

    build-darwin

--- build_darwin.sh
+++ build_darwin.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 cd "$( dirname "$0" )"
 cd zipmanager
-python3 -m PyInstaller "Darwin.spec" --windowed
+rm -rf build dist
+python3 -m PyInstaller --onefile "Darwin.spec"
 cd dist/SomePythonThings\ Zip\ Manager.app/Contents/MacOS
 sudo codesign --remove-signature "SomePythonThings Zip Manager"
+cd ../../../
+pwd
+ls
 echo
 mv "SomePythonThings Zip Manager.app" ../../
